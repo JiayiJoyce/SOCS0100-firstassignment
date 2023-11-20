dd5f6d6c404cee650ab08ce64c40033764649734
JiayiJoyce <jiayijoyce@163.com>
2023-11-20 23:59:12 +0000

    added theme

--- SOCS0100_code.R
+++ SOCS0100_code.R
@@ -2,15 +2,15 @@
 # 0. Set up: installing packages, remove the # in front of them to run-----
 
 #set working directory
-#setwd("copy directory address here") 
+setwd("/Users/chenjiayi/Desktop/Computational/DVHZ3-SOCS0100-Assignment")
 
 #clearing environment
-#rm(list = ls())
+rm(list = ls())
 
 #install Groundhog if it is not installed, remove # to run
-# if (!require("groundhog")) {
-#   install.packages("groundhog")
-# } 
+ if (!require("groundhog")) {
+install.packages("groundhog")
+}
 
 #creating the vector pkgs that include all the packages needed for the following task
 pkgs <- c("tidyverse", 
@@ -23,11 +23,23 @@ pkgs <- c("tidyverse",
           "countrycode")
 
 # install packages as they are available on 2023-11-04, remove # to install
-#groundhog.library(pkgs, "2023-11-04")
+groundhog.library(pkgs, "2023-11-04")
+install.packages("devtools")
+install.packages("sysfonts")
+install.packages("showtext")
+
+library(devtools)
+library(sysfonts)
+library(showtext)
+
+# install.packages("remotes")
+remotes::install_github("MatthewBJane/ThemePark")
+library(ThemePark)
+head(themepark_themes)
 
 # 1. Part I-A Data Exploration and contextualisation ------------
 #importing data
-energy_and_fuel <- read_csv("Number of people with and without energy access (OWID based on World Bank, 2021).csv")
+energy_and_fuel <- read_csv("/Users/chenjiayi/Desktop/Computational/DVHZ3-SOCS0100-Assignment/Number of people with and without energy access (OWID based on World Bank, 2021).csv")
 #inspecting data
 str(energy_and_fuel)
 summary(energy_and_fuel)
@@ -94,13 +106,16 @@ top_five_population <- country_percentage_energy_and_fuel %>%
 options(scipen=999)
 create_point_plot <- function(i) {
   income_based_entity %>%
-    ggplot(aes_string(x = names(income_based_entity)[2], y = names(income_based_entity)[i]),linetype = entity) +
+    ggplot(aes_string(x = names(income_based_entity)[2], y = names(income_based_entity)[i]),
+           color = "entity") +
     geom_point() +
     geom_smooth(method = "lm", se = TRUE, color = "purple") +  # Add trend lin
     labs(
       title = glue("The Trend of {names(income_based_entity)[i]}"),
       y = glue("{names(income_based_entity)[i]}")
-    )
+    )+
+    scale_color_manual(values = c("blue", "red", "green","yellow","black"))
+    
 }
 
 plots_list <- map(3:ncol(income_based_entity), create_point_plot)
@@ -110,7 +125,10 @@ plots_list
 
 # 4. Data visualisation for top five population countries----
 options(scipen=999)
-ggplot(top_five_population, aes(x = year, y = percentage_with_electricity_access , linetype = country)) + geom_point() +
-  geom_smooth(method = "lm", se = TRUE, color = "purple") 
+ggplot(top_five_population, aes(x = year, y = percentage_with_electricity_access , linetype = country)) + geom_point(colour="pink"
+  ) +
+  geom_smooth(method = "lm", se = TRUE
+              ,colour="skyblue"
+              ) +theme_friends()
 geom_smooth()
   
